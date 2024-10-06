9143336189a908e61f72a03b6e78b46eec0f00ee
dcirulli89 <92664275+dcirulli89@users.noreply.github.com>
2024-10-06 03:06:07 -0300

    Create firebase.js

---
+++ frontend/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCn9s1RUeLqNud7bj5Ifz6eLilHicCUE_c",
+  authDomain: "atividade-extensionista-02.firebaseapp.com",
+  projectId: "atividade-extensionista-02",
+  storageBucket: "atividade-extensionista-02.appspot.com",
+  messagingSenderId: "788663870213",
+  appId: "1:788663870213:web:21c57ddf284a6be095cd6c",
+  measurementId: "G-4V32CQFJFG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
